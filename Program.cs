55da255bde05be3fa2815b1760daa716c6b6a8df
yunasim <ynsim@solidit.co.kr>
2023-06-13 00:47:41 +0900

    #✨feature: 싱글톤 패턴 구현 및 샘플 구현

---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonPattern
+{
+    public class Program
+    {
+        static SingletonPattern m_SingletonPattern;
+
+        static void Main(string[] args)
+        {
+            // 싱글톤 패턴 초기화
+            m_SingletonPattern = SingletonPattern.Instance;
+
+            // 싱글톤 패턴에 데이터 1차 삽입
+            m_SingletonPattern.SetData("Main 데이터", "1차");
+            m_SingletonPattern.ShowData();
+
+            new TempClass();
+            m_SingletonPattern.ShowData();
+
+            new TempClass2();
+            m_SingletonPattern.ShowData();
+        }
+    }
+
+    public class TempClass
+    {
+        SingletonPattern m_SingletonPattern;
+
+        public TempClass()
+        {
+            m_SingletonPattern = SingletonPattern.Instance;
+
+            // 싱글톤 패턴에 데이터 2차 삽입
+            m_SingletonPattern.SetData("TempClass 데이터", "2차");
+
+        }
+    }
+
+    public class TempClass2
+    {
+        SingletonPattern m_SingletonPattern;
+
+        public TempClass2()
+        {
+            m_SingletonPattern = SingletonPattern.Instance;
+
+            // 싱글톤 패턴에 데이터 3차 삽입
+            m_SingletonPattern.SetData("TempClass2 데이터", "3차");
+
+        }
+    }
+
+}
---
+++ SingletonPattern.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonPattern
+{
+    public class SingletonPattern
+    {
+        public string m_strName { get; private set; }
+        public string m_strDesc { get; private set; }
+
+        private static SingletonPattern Inst;
+
+        public static SingletonPattern Instance
+        {
+            get
+            {
+                if (Inst == null)
+                    Inst = new SingletonPattern();
+                return Inst;
+            }
+        }
+
+        private SingletonPattern()
+        {
+            m_strName = "";
+            m_strDesc = "";
+        }
+
+        public void SetData(string strName, string strDesc)
+        {
+            m_strName = strName;
+            m_strDesc = strDesc;
+        }
+
+        public void ShowData()
+        {
+            Console.WriteLine("Name: {0}, Desc: {1}", m_strName, m_strDesc);
+        }
+    }
+}
